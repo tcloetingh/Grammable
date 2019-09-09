64ecb54753a3b03b26124c061f7f5e63ed6ad3fe
Tyler <tcloetingh@gmail.com>
2019-09-09 20:42:08 +0000

    carrierwave bug

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,12 +1,22 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :show, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :show, :edit, :update, :destroy]
 
   def index
+    @grams = Gram.all 
+  end
+
+  def destroy
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
+    @gram.destroy
+    redirect_to root_path
   end
 
   def update
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
 
     @gram.update_attributes(gram_params)
 
@@ -20,6 +30,7 @@ class GramsController < ApplicationController
   def edit
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
   end
 
   def show
@@ -43,11 +54,11 @@ class GramsController < ApplicationController
   private
 
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
 
-  def render_not_found
-    render plain: 'Not Found :(', status: :not_found 
+  def render_not_found(status=:not_found)
+    render plain: "#{status.to_s.titleize} :(", status: status
   end
 
 end
