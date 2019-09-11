4d476befccc7b1dddf5aee9919728e4ded940293
Tyler <tcloetingh@gmail.com>
2019-09-11 23:29:26 +0000

    index/edit/session styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   root "grams#index"
-    resources :grams, only: [:new, :create, :show, :edit, :update, :destroy]
-    resources :comments, only: [:create]
+    resources :grams do
+      resources :comments, only: [:create]
+    end
 end
